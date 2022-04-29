964306dcda048833186d9bd9d5f33def79eb2bd0
Grzegorz Walczak <Grzesiek.M.Walczak@gmail.com>
2022-04-29 14:06:16 +0200

    Hero section styling changes

--- src/features/Home/components/Sections/HeroSection/HeroSection.tsx
+++ src/features/Home/components/Sections/HeroSection/HeroSection.tsx
@@ -1,51 +1,30 @@
 import Image from 'next/image';
 import styles from './HeroSection.module.css';
-import Murray_Rothbard from '../../../../../../public/images/backgrounds/Murray_Rothbard.png';
-// import checkered from '../../../../../public/images/backgrounds/checkered.svg';
-import { Button } from '../../../../../components/Button/';
+import Murray_Rothbard from '@/public/images/backgrounds/Murray_Rothbard.png';
+import { Button } from '@/components/Button/';
 
 export const HeroSection = () => {
   return (
-    <>
-      <div className={styles.murray}>
-        <Image src={Murray_Rothbard} alt="Murray Rothbard" />
+    <div className={styles.container}>
+      <div className={styles.background}>
+        <div className={styles.img}>
+          <Image src={Murray_Rothbard} alt="Murray Rothbard"/>
+        </div>
       </div>
-      {/* <div className={styles.checkered}>
-        <Image src={checkered} alt="background" />
-      </div> */}
-      <div className={`card border-0 bg-transparent ${styles[`hero-card`]}`}>
-        <div className="card-body">
-          <h5 className="card-title">Lorem ipsum</h5>
-          <h6 className="card-subtitle">
+      <div className={styles.card}>
+          <div className={styles.title}>Lorem ipsum</div>
+          <div className={styles.subtitle}>
             iusto odio dignissimos ducimus qui blanditiis
-          </h6>
-          <p className="card-text">
+          </div>
+          <p>
             Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil
             impedit quo minus.
           </p>
-          {/* <button
-            type="button"
-            className={`btn rounded-pill ${styles[`custom-btn`]} ${
-              styles[`ylw`]
-            }`}
-          >
-            <a href="#" className="card-link text-dark text-decoration-none"> */}
-          <Button variant="yellow">Consequatur</Button>
-          {/* </a>
-          </button> */}
-          {/* <button
-            type="button"
-            className={`btn btn-light btn-outline-dark rounded-pill ${
-              styles[`custom-btn`]
-            }`}
-            style={{ width: `140px` }}
-          >
-            <a href="#" className="card-link text-dark text-decoration-none"> */}
-          <Button variant="black">Similique sunt</Button>
-          {/* </a>
-          </button> */}
-        </div>
+          <div className={styles.button}>
+            <Button variant="yellow">Consequatur</Button>
+            <Button variant="black">Similique sunt</Button>
+          </div>
       </div>
-    </>
-  );
+    </div>
+    );
 };
